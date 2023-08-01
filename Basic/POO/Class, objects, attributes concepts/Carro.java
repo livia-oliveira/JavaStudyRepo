d817d91ce894d7ea95b970983e4c97ea2c0de11e
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-08-01 15:03:12 -0300

    Class, objects, attributes concepts

---
+++ Basic/POO/Class, objects, attributes concepts/Carro.java
@@ -0,0 +1,8 @@
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+}
---
+++ Basic/POO/Class, objects, attributes concepts/testeCarro.java
@@ -0,0 +1,16 @@
+public class testeCarro {
+    public static void main(String[] args) {
+        Carro van = new Carro();
+
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.capCombustivel = 100;
+        van.consumoCombustivel = 0.2;
+
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+
+
+    }
+}
