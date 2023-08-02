f9f360fc38d09c94f4183fdc2473b7d527e9843b
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-08-02 14:55:35 -0300

    Exercise 2 - Book

---
+++ Basic/POO/Exercises classes and attributes/Livro.java
@@ -0,0 +1,7 @@
+public class Livro {
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+
+}
---
+++ Basic/POO/Exercises classes and attributes/testeLivro.java
@@ -0,0 +1,11 @@
+public class Exer02 {
+    public static void main(String[] args) {
+        Livro livro = new Livro();
+
+        livro.nome = "O nome da rosa";
+        livro.autor = "Umberto Eco";
+        livro.qtdPaginas = 300;
+        livro.anoLancamento = 1980;
+
+    }
+}
