a93c2e75f8885f3f89060aa23ef58773d20a1c4b
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-08-20 11:30:59 -0300

    Method and constructor overloading

---
+++ Basic/POO/Method and constructor overloading/testeCalculadora.java
@@ -0,0 +1,10 @@
+public class testeCalculadora {
+
+    public static void main(String[] args) {
+        minhaCalculadora calc = new minhaCalculadora();
+
+        calc.soma(1, 2);
+        calc.soma(1.5, 1.8);
+        calc.soma(1, 2, 3);
+    }
+}
