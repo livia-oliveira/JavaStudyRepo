6a52a2a2ad2e0633210d6bf87a6733b20bbd952a
livia-oliveira <livia0liveiraslv93@gmail.com>
2023-08-19 22:33:48 -0300

    Get and set methods

---
+++ Basic/POO/Get and set methods/testeCarro.java
@@ -0,0 +1,8 @@
+public class testeCarro {
+    public static void main(String[] args) {
+        Carro van = new Carro();
+
+        van.setMarca("Fiat");
+        System.out.println(van.getMarca());
+    }
+}
